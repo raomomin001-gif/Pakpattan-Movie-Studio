4326d35a105c4de2652a78447f00fcccb9344f66
raomomin001-gif <raomomin001@gmail.com>
2026-01-06 00:39:26 +0500

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  var audio = document.getElementById("autoVoice");
+
+  audio.play().catch(function () {
+    document.body.addEventListener("click", function () {
+      audio.play();
+    }, { once: true });
+  });
+
+});
 
